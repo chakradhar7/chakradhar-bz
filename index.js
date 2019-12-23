d8b616a10350637a840807efcb2a649db131a484
chakradhar7 <chakradhar729@gmail.com>
2019-12-23 05:57:04 +0530

    designed html page with bootstrap

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const port = process.env.PORT||3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
 
+var obj = {}
+obj.name = "Tadepalli Venkata Sai Chakradhar";
+obj.college = "CMR College Of Engineering & Technology";
+obj.rollno = "17H51A04P4";
+
+app.get('/data',(req,res) => res.json(obj))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
